a15e81706c649fabadc22d4e903c67fe8d41fa48
efuentes21 <edgar.fuentes@inslapineda.cat>
2024-03-05 09:43:00 +0100

    rutas

--- docker/apache_web/strix/routes/web.php
+++ docker/apache_web/strix/routes/web.php
@@ -22,17 +22,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin/races', [RaceController::class, 'index']);
+Route::get('admin/races', [RaceController::class, 'index'])->name('race.index');
 Route::get('race/create', [RaceController::class, 'create'])->name('race.create');
 
-Route::get('admin/sponsors', [SponsorController::class, 'index']);
+Route::get('admin/sponsors', [SponsorController::class, 'index'])->name('sponsor.index');
 Route::get('sponsor/create', [SponsorController::class, 'create'])->name('sponsor.create');
 
-Route::get('admin/insurances', [InsuranceController::class, 'index']);
+Route::get('admin/insurances', [InsuranceController::class, 'index'])->name('insurance.index');
 Route::get('insurance/create', [InsuranceController::class, 'create'])->name('insurance.create');
 
-Route::get('admin/challenges', [ChallengeController::class, 'index']);
+Route::get('admin/challenges', [ChallengeController::class, 'index'])->name('challenge.index');
 Route::get('challenge/create', [ChallengeController::class, 'create'])->name('challenge.create');
 
-Route::get('admin/competitors', [CompetitorController::class, 'index']);
+Route::get('admin/competitors', [CompetitorController::class, 'index'])->name('competitor.index');
 Route::get('competitor/create', [CompetitorController::class, 'create'])->name('competitor.create');
